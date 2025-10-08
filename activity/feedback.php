80d130ecb01dcc1eff86327580617569e343d3e1
hakimks <hakimks@gmail.com>
2025-10-08 17:46:50 +0300

    OM-828 - fix Warning: Array to string conversion. Add Array to string conversion if an array is detected

--- activity/feedback.php
+++ activity/feedback.php
@@ -58,7 +58,11 @@ class MobileActivityFeedback extends MobileActivity {
     private function generate_md5($feedback, $quizjson) {
         $md5postfix = "";
         foreach ($this->configarray as $key => $value) {
-            $md5postfix .= $key[0].((string) $value);
+            if (is_array($value)) {
+                $md5postfix .= $key[0] . json_encode($value);
+            } else {
+                $md5postfix .= $key[0] . ((string) $value);
+            }
         }
         $contents = json_encode($quizjson);
         $this->md5 = md5( $feedback->intro . remove_ids_from_json($contents) . $md5postfix);
